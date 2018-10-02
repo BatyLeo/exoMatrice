0416ced7fa84b7747b598f4def1309fc4e9bee2a
BatyLeo <leo.baty@eleves.enpc.fr>
2018-10-02 12:58:54 +0200

    version finale

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include <ctime>
+#include <random>
+#include <cstdlib>
+
+#include "matrice.h"
+
 
 
 int main() {
-    std::cout << "Hello world !" << std::endl;
+
+    srand(time(NULL));
+
+    int n = 1;
+    for (int i = 0; i < n; i++)
+    {
+        Matrice A(rand()%10 + 1, rand()%10 + 1);
+        //A.affiche();
+        Matrice B(rand()%10 + 1, A.get_n());
+        //B.affiche();
+
+        Matrice C = B * A;
+        C.affiche();
+
+        const Matrice D = C;
+        D.affiche();
+
+        A = D;
+    }
+
     return 0;
 }
